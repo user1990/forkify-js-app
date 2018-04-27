432110e2095751a4a4d79afa9cbf2ef8e461e953
user1990 <paulius.rimg1990@gmail.com>
2018-04-27 09:57:52 +0300

    Building the Shopping List View

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,6 +5,7 @@ export const elements = {
   searchResultList: document.querySelector('.results__list'),
   searchResultPages: document.querySelector('.results__pages'),
   recipe: document.querySelector('.recipe'),
+  shopping: document.querySelector('.shopping__list'),
 };
 
 export const elementStrings = {
